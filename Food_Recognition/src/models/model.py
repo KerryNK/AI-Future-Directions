7ec63db3a55e12251d6d4cf2f20098f92de559af
Ciarrai <163757149+KerryNK@users.noreply.github.com>
2025-06-24 03:06:39 +0300

    Create model.py

---
+++ Food_Recognition/src/models/model.py
@@ -0,0 +1,18 @@
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense
+from tensorflow.keras.models import Model
+
+def create_model(num_classes):
+    base_model = MobileNetV2(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+    
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(1024, activation='relu')(x)
+    predictions = Dense(num_classes, activation='softmax')(x)
+
+    model = Model(inputs=base_model.input, outputs=predictions)
+
+    return model
+
+def compile_model(model):
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
